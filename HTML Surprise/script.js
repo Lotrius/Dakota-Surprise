99bfd0a9e581acf78d6fa2bba995fbc060aeeeb0
Somesk278 <user.email>
2018-07-03 01:30:26 +0100

    added js

--- HTML Surprise/script.js
+++ HTML Surprise/script.js
@@ -4,7 +4,7 @@ const pic = document.getElementsByClassName("pic");
 const addPic = () => {
   if(pic[0].childNodes.length < 2) {
     const img = document.createElement("img");
-    img.src = "ocelot1.jpg";
+    img.src = "https://rlv.zcache.com/you_dont_look_an_ocelot_older_birthday_card-r33063ba37aab4f7bb091562a241c65ae_xvuat_8byvr_540.jpg";
     img.alt = "Happy birthday ocelot";
     img.height = 150;
     img.width = 150;
